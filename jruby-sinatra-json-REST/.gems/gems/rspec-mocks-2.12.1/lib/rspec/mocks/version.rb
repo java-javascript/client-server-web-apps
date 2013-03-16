131dba59b7a2f1fa20e419ca8a2cd1f668c391a5
C S <cs@Cs-MacBook-Air.(none)>
2013-03-16 11:32:53 -0400

    cleanup

--- jruby-sinatra-json-REST/.gems/gems/rspec-mocks-2.12.1/lib/rspec/mocks/version.rb
+++
@@ -1,7 +0,0 @@
-module RSpec
-  module Mocks
-    module Version
-      STRING = '2.12.1'
-    end
-  end
-end
--- jython-json-REST/rest-json/python/http_server.py
+++
@@ -1,8 +0,0 @@
-import SimpleHTTPServer
-import SocketServer
-import os
-
-os.chdir('src/main/resources')
-httpd = SocketServer.TCPServer(("", 8000), SimpleHTTPServer.SimpleHTTPRequestHandler)
-print "serving at port 8000"
-httpd.serve_forever()
