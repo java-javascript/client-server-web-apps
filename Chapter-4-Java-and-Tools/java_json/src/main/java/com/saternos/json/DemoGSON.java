d8fa429be17e11eb4e8b5c945d2536148a26e3b6
C S <cs@Cs-MacBook-Air.(none)>
2013-05-06 18:01:36 -0400

    Added Java/JSON examples

---
+++ Chapter-4-Java-and-Tools/java_json/src/main/java/com/saternos/json/DemoGSON.java
@@ -0,0 +1,26 @@
+package com.saternos.json;
+
+import com.google.gson.Gson;
+
+/**
+ *
+ */
+public class DemoGSON {
+
+  public static void main(String args[]) {
+
+    MyPojo pojo = new MyPojo();
+    pojo.setThing1("Hello");
+    pojo.setThing2("GSON");
+
+    Gson gson = new Gson();
+    String json = gson.toJson(pojo);
+    System.out.println(json);
+
+    System.out.println("\nNow create object and populate it with the JSON string.");
+    MyPojo pojo2  = gson.fromJson(json, MyPojo.class);
+    System.out.println("Serialized and deserialized object are equal: " + pojo.equals(pojo2));
+
+  }
+
+}
---
+++ Chapter-4-Java-and-Tools/java_json/src/main/java/com/saternos/json/DemoJackson.java
@@ -0,0 +1,26 @@
+package com.saternos.json;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+/**
+ *
+ */
+public class DemoJackson {
+
+  public static void main (String args[]) throws Exception{
+
+    MyPojo pojo = new MyPojo();
+    pojo.setThing1("Hello");
+    pojo.setThing2("Jackson");
+
+    ObjectMapper mapper = new ObjectMapper();
+    String json = mapper.writeValueAsString(pojo);
+    System.out.println(json);
+
+    System.out.println("\nNow create object and populate it with the JSON string.");
+    MyPojo pojo2  = mapper.readValue(json, MyPojo.class);
+    System.out.println("Serialized and deserialized object are equal: " + pojo.equals(pojo2));
+
+  }
+
+}
