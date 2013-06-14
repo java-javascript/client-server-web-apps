827eb504bc1190d1702c3ad47e9b940a3d4e5125
C S <cs@cs-macbook-air.example.com>
2013-06-14 12:30:14 -0400

    Added Restlet example

---
+++ Chapter-6-Java-RESTful-Servers/restlet-server/src/main/java/com/saternos/embedded/TestRestletHttpServer.java
@@ -0,0 +1,22 @@
+package com.saternos.embedded;
+
+import org.restlet.*;
+import org.restlet.data.*;
+import org.restlet.resource.*;
+import org.restlet.ext.json.JsonConverter;
+
+public class TestRestletHttpServer extends ServerResource {
+
+	public static void main(String[] args) throws Exception {  
+	    Component component = new Component();  
+	    component.getServers().add(Protocol.HTTP, Integer.parseInt(args[0]));  
+	    component.getDefaultHost().attach("/", TestRestletHttpServer.class);  
+	    component.start();  
+	}  
+
+	@Get ("json")
+	public String getGreeting() {  
+	    return "{\"testResponse\":\"Hello World\"}";  
+	}
+	
+}
