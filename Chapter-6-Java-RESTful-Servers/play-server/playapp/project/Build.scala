1f7069d3a54de098ba4f8f63cd87ccb514b10c8c
C S <cs@Cs-MacBook-Air.(none)>
2013-06-18 20:31:49 -0400

    Cleanup

--- Chapter-6-Java-RESTful-Servers/play-server/playapp/project/Build.scala
+++ Chapter-6-Java-RESTful-Servers/play-server/playapp/project/Build.scala
@@ -4,7 +4,7 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "junk"
+  val appName         = "play-server"
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
