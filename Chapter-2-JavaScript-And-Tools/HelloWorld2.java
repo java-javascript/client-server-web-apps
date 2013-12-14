7dcacabeaa7bd818bbfb90fd161cb8494948f978
Cas <cas.saternos@gmail.com>
2013-12-13 21:29:40 -0500

    Update HelloWorld2.java

--- Chapter-2-JavaScript-And-Tools/HelloWorld2.java
+++ Chapter-2-JavaScript-And-Tools/HelloWorld2.java
@@ -1,10 +1,11 @@
 /**
  * HelloWorld2
  */
-class HelloWorld2{
+class HelloWorld2 {
 
-    public static void main (String args[]){
+    public static void main (String args[]) {
         String name;
         System.out.println("Hello " + name);
+    }    
   
 }
