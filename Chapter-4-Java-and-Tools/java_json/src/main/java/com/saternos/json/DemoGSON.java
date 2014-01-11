43da2bc8e8faabc3c2edae0c4551de0f66ff628d
Cas S <ezgraphs@gmail.com>
2014-01-10 19:27:46 -0500

    Formatted code

--- Chapter-4-Java-and-Tools/java_json/src/main/java/com/saternos/json/DemoGSON.java
+++ Chapter-4-Java-and-Tools/java_json/src/main/java/com/saternos/json/DemoGSON.java
@@ -7,20 +7,20 @@ import com.google.gson.Gson;
  */
 public class DemoGSON {
 
-  public static void main(String args[]) {
+    public static void main(String args[]) {
 
-    MyPojo pojo = new MyPojo();
-    pojo.setThing1("Hello");
-    pojo.setThing2("GSON");
+        MyPojo pojo = new MyPojo();
+        pojo.setThing1("Hello");
+        pojo.setThing2("GSON");
 
-    Gson gson = new Gson();
-    String json = gson.toJson(pojo);
-    System.out.println(json);
+        Gson gson = new Gson();
+        String json = gson.toJson(pojo);
+        System.out.println(json);
 
-    System.out.println("\nNow create object and populate it with the JSON string.");
-    MyPojo pojo2  = gson.fromJson(json, MyPojo.class);
-    System.out.println("Serialized and deserialized object are equal: " + pojo.equals(pojo2));
+        System.out.println("\nNow create object and populate it with the JSON string.");
+        MyPojo pojo2 = gson.fromJson(json, MyPojo.class);
+        System.out.println("Serialized and deserialized object are equal: " + pojo.equals(pojo2));
 
-  }
+    }
 
 }
--- Chapter-4-Java-and-Tools/java_json/src/main/java/com/saternos/json/MyPojo.java
+++ Chapter-4-Java-and-Tools/java_json/src/main/java/com/saternos/json/MyPojo.java
@@ -4,46 +4,46 @@ package com.saternos.json;
  *
  */
 public class MyPojo {
-  private String thing1;
-  private String thing2;
+    private String thing1;
+    private String thing2;
 
-  public MyPojo(){
-    System.out.println("*** Constructor MyPojo() called");
-  }
+    public MyPojo() {
+        System.out.println("*** Constructor MyPojo() called");
+    }
 
-  public String getThing1() {
-    return thing1;
-  }
+    public String getThing1() {
+        return thing1;
+    }
 
-  public void setThing1(String thing1) {
-    this.thing1 = thing1;
-  }
+    public void setThing1(String thing1) {
+        this.thing1 = thing1;
+    }
 
-  public String getThing2() {
-    return thing2;
-  }
+    public String getThing2() {
+        return thing2;
+    }
 
-  public void setThing2(String thing2) {
-    this.thing2 = thing2;
-  }
+    public void setThing2(String thing2) {
+        this.thing2 = thing2;
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
-    MyPojo myPojo = (MyPojo) o;
+        MyPojo myPojo = (MyPojo) o;
 
-    if (thing1 != null ? !thing1.equals(myPojo.thing1) : myPojo.thing1 != null) return false;
-    if (thing2 != null ? !thing2.equals(myPojo.thing2) : myPojo.thing2 != null) return false;
+        if (thing1 != null ? !thing1.equals(myPojo.thing1) : myPojo.thing1 != null) return false;
+        if (thing2 != null ? !thing2.equals(myPojo.thing2) : myPojo.thing2 != null) return false;
 
-    return true;
-  }
+        return true;
+    }
 
-  @Override
-  public int hashCode() {
-    int result = thing1 != null ? thing1.hashCode() : 0;
-    result = 31 * result + (thing2 != null ? thing2.hashCode() : 0);
-    return result;
-  }
+    @Override
+    public int hashCode() {
+        int result = thing1 != null ? thing1.hashCode() : 0;
+        result = 31 * result + (thing2 != null ? thing2.hashCode() : 0);
+        return result;
+    }
 }
