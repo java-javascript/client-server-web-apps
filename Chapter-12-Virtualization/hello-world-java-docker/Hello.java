7d6d4ff01246113bcb80444251239084b0fcbd88
Cas S <ezgraphs@gmail.com>
2014-01-10 19:44:28 -0500

    Formatted code

--- Chapter-12-Virtualization/hello-world-java-docker/Hello.java
+++ Chapter-12-Virtualization/hello-world-java-docker/Hello.java
@@ -1,5 +1,5 @@
-public class Hello{
-  public static void main (String args[]){
-    System.out.println("hey there from java");
-  }
+public class Hello {
+    public static void main(String args[]) {
+        System.out.println("hey there from java");
+    }
 }
